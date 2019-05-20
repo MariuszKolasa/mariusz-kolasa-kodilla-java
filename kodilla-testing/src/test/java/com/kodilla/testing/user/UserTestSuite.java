375c3e85d37699d23951cda4d80f850d589be53b
MariuszKolasa <mariusz.kolasa89@gmail.com>
2019-05-20 11:31:38 +0200

    commit21maj

--- kodilla-testing/src/test/java/com/kodilla/testing/user/UserTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/user/UserTestSuite.java
@@ -1,7 +1,7 @@
-package com.kodilla.testing.forum;
+package com.kodilla.testing.user;
 import com.kodilla.testing.user.SimpleUser;
 import org.junit.*;
-public class ForumTestSuite {
+public class UserTestSuite {
     @Before
     public void before(){
         System.out.println("Test Case: begin");
